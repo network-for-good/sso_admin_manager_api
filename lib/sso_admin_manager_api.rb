e4067281bf5facfe38b983c0f145299aeec54219
Thomas Hoen <tom@givecorps.om>
2017-02-10 11:30:05 -0500

    explicitly require responder gem

--- lib/sso_admin_manager_api.rb
+++ lib/sso_admin_manager_api.rb
@@ -1,6 +1,7 @@
 require "sso_admin_manager_api/engine"
 require "sso_admin_manager_api/token_decoder"
 require "sso_admin_manager_api/secure_api"
+require "responders"
 
 module SsoAdminManagerApi
 end
